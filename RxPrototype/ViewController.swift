694bb8cbdd9af56d576ccdbeb71d8a04b1ae7cee
Michael Long <mlong@clientresourcesinc.com>
2018-06-02 09:24:44 -0500

    Initial Commit

---
+++ RxPrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxPrototype
+//
+//  Created by Michael Long on 6/2/18.
+//  Copyright © 2018 Michael Long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
